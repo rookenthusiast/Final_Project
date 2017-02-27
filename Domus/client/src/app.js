a9552520654d9b91663fd14fff5ccd7966414821
Cameron Fulton <rookenthusiast@gmail.com>
2017-02-27 11:27:23 +0000

    removed react stuff in app

--- Domus/client/src/app.js
+++ Domus/client/src/app.js
@@ -1,15 +1,6 @@
-import SettlementView from './containers/SettlementView.jsx'
-import WorldMap from './containers/WorldMap.jsx'
-import UserPortal from './containers/UserPortal.jsx'
 
 
 window.onload = function(){
     <h1> App Started </h1>,
-    <div>
-    <SettlementView/>
-    <WorldMap/>
-    <UserPortal/>
-    </div>,
-    document.getElementById('app')
-  );
+    document.getElementById('app');  
 }
