4dbd7a85d9e8d84aee28a40354c2b76906b5843e
Cameron Fulton <rookenthusiast@gmail.com>
2017-02-27 15:53:28 +0000

    model changes, changed constructor in village

--- Domus/client/src/models/Village.js
+++ Domus/client/src/models/Village.js
@@ -1,17 +1,18 @@
 var json = require('../../../village.json');
 var unit = require('./Unit.js');
-// var resource = require('./Resource.js');
-// var race = require('./Race.js');
+var resource = require('./Resource.js');
+var race = require('./Race.js');
 
 class Village {
 
-  constructor(owned_by, buildings, coordinates, resources, race, units) {
+  constructor(owned_by, coordinates, resources, race) {
     this.owned_by = owned_by;
-    this.buildings = buildings;
+    this.buildings = [];
+    // coordinates is an object {x:1,y:2}
     this.coordinates = coordinates;
     this.resources = resources;
     this.race = race;
-    this.units = units;
+    this.garrison = [];
   }
 }
 
